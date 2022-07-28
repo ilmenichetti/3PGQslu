ede5bff3bb786bad9a151ad813c70129ff646d15
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-07-28 16:37:57 +0100

    update

--- Rsrc/extractWeather.r
+++ Rsrc/extractWeather.r
@@ -16,6 +16,7 @@ use_python("C:/Python27")
 source_python("Rsrc/clipick.py")
 ##if error:
 # devtools::install_github("rstudio/reticulate")
+# reticulate::install_miniconda()
 
 
 coord <- data.table(lat=56,long=13)
