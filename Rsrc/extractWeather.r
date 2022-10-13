d0e2b1d79816f4aa46f4e55b88992652076e346d
ForModLabUHel <francesco.minunno@helsinki.fi>
2022-10-13 12:15:46 +0100

    Update extractWeather.r
    test

--- Rsrc/extractWeather.r
+++ Rsrc/extractWeather.r
@@ -18,7 +18,6 @@ source_python("Rsrc/clipick.py")
 # devtools::install_github("rstudio/reticulate")
 # reticulate::install_miniconda()
 
-
 coord <- data.table(lat=56,long=13)
 ciao <- getWDpoints(coord)
 ciao
